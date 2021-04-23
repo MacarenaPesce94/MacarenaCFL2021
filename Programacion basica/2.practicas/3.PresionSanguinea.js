1d6384dc8b3e0a604f17fb91d6443b2f0803c8a7
MacarenaPesce94 <macarenapesce94@gmail.com>
2021-04-22 21:21:34 -0300

    Ejercicios clase 2 seleccion

---
+++ Programacion basica/2.practicas/3.PresionSanguinea.js
@@ -0,0 +1,5 @@
+console.log("Tomar la presion sanguinea");
+console.log("Entrada: Paciente/Brazo del Paciente");
+console.log("Proceso: Colocar el tensiometro");
+console.log("Proceso: Tomas la presion");
+console.log("Salida: Resultado del tensiometro");
---
+++ Programacion basica/2.practicas/4.Sumar.js
@@ -0,0 +1,4 @@
+console.log("sumar dos numeros");
+console.log("Entrada: los dos numeros a sumar");
+console.log("Proceso: realizar la suma de los dos numeros");
+console.log("Salida: el resultado de la suma");
