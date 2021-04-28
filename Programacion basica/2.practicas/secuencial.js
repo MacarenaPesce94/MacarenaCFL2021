31f04a70d91d5cacd1574f1483647e2e3a125a78
MacarenaPesce94 <macarenapesce94@gmail.com>
2021-04-27 21:13:33 -0300

    Ejercicios clase 2 seleccion

---
+++ Programacion basica/2.practicas/secuencial.js
@@ -0,0 +1,5 @@
+let readlineSync = require("readline-sync");
+let primerPalabra = readlineSync.question("Este");
+let segundaPalabra = readlineSync.question("Algoritmo");
+let tercerPalabra = readlineSync.question("es");
+let cuartaPalabra = readlineSync.question("secuencial");
